4bd3d85bed0b4f829e6889e5801cbb50fce15899
vladislavmihajlenko <mykhailenko.vladislav@gmail.com>
2023-06-04 14:47:05 -0600

    Update 4-06

--- ChekGmail.py
+++ ChekGmail.py
@@ -1,25 +1,28 @@
 # This script by Mykhailenko Vladyslav 😁 https://github.com/VladyslavMykhailenko
 
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def delay():
+    time.sleep(random.randint(2, 4))
 
 driver = webdriver.Chrome()
 #check gmail
 driver.get("https://www.google.com/account/about/")
 driver.maximize_window()
-time.sleep(1)
+delay()  # simulate long running test
 driver.find_element(By.PARTIAL_LINK_TEXT, "Go to Google Account").click()
 driver.find_element(By.ID, "identifierId").send_keys("qat0368@gmail.com")
-time.sleep(4)
+delay()  # simulate long running test
 driver.find_element(By.XPATH, "//*[text()='Next']").click()
-time.sleep(4)
+delay()  # simulate long running test
 driver.find_element(By.XPATH, "//*[@type='password']").send_keys("gfhjkmgfhjkm!`")
-time.sleep(2)
+delay()  # simulate long running test
 driver.find_element(By.XPATH, "//*[text()='Next']").click()
-time.sleep(1)
+delay()  # simulate long running test
 driver.get("https://mail.google.com/mail/")
-
+delay()  # simulate long running test
 # closing the browser
 driver.quit()
